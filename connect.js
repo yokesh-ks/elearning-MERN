d0c2b006ca2fa39729a5fa639439afcd7e2dbdec
YOKESH K S <ksyokesh98@gmail.com>
2021-10-22 13:36:17 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToDB = async () => {
-    mongoose.connect(process.env.mongodb_url, {
+    mongoose.connect("mongodb+srv://yokesh:yokesh123@apidev.e8ljl.mongodb.net/elearning?retryWrites=true&w=majority", {
         useNewUrlParser: true,
      // useFindAndModify: false,
         useUnifiedTopology: true,
