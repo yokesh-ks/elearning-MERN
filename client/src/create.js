10c66ad2a5f2362d38c80ffc830ab6d59ba3b960
yokesh-ks <ksyokesh98@gmail.com>
2022-02-27 00:07:40 +0530

    Code CleanUp

--- client/src/create.js
+++ client/src/create.js
@@ -1,97 +1,107 @@
-import './css/style.css';
+import "./css/style.css";
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
-import axios from 'axios';
+import axios from "axios";
 
-function Create(){
-    const [youtubeVideo, setYoutubeVideo] = useState('');
-    const [youtubeURL, setYoutubeURL] = useState('');
-    const [youtubeError, setYoutubeError] = useState('');
-    const handleYoutubeChange=(e)=>{
-        setYoutubeVideo(e.target.value);
+function Create() {
+  const [youtubeVideo, setYoutubeVideo] = useState("");
+  const [youtubeURL, setYoutubeURL] = useState("");
+  const [youtubeError, setYoutubeError] = useState("");
+  const handleYoutubeChange = (e) => {
+    setYoutubeVideo(e.target.value);
+  };
+  const [video, setVideo] = useState([
+    {
+      title: "",
+      description: "",
+    },
+  ]);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setVideo((prevInput) => {
+      return {
+        ...prevInput,
+        [name]: value,
+      };
+    });
+  }
+
+  async function publish(e) {
+    e.preventDefault();
+    try {
+      const newVideo = {
+        insertVideo: {
+          video: {
+            title: video.title,
+            description: video.description,
+            url: youtubeURL,
+          },
+        },
+      };
+      console.log(newVideo);
+      await axios.put("/addvideo/617252ce83f998658c61d1a0", newVideo);
+      alert("video added added");
+    } catch (error) {
+      console.log(error);
     }
-    const [video, setVideo] = useState ([
-        {
-            title:'',
-            description: ''
-        }
-    ])
+  }
 
-    function handleChange(e) {
-        const {name, value} = e.target;
-        setVideo(prevInput => {
-          return(
-            {
-              ...prevInput,
-              [name]: value
-            }
-          )
-        })
-      }
-      
-      async function publish(e) {  
-        e.preventDefault();
-        try{
-            const newVideo = {
-                insertVideo:
-                {
-                    video:
-                        {
-                            title: video.title,
-                            description: video.description,
-                            url: youtubeURL
-                        }
-                }
-            }
-            console.log(newVideo);
-            await axios.put('/addvideo/617252ce83f998658c61d1a0', newVideo);
-            alert("video added added")
-        } catch(error){
-            console.log(error);
-        }
-        
-      }
-
-    const handleYoutubeSubmit=(e)=>{
-        e.preventDefault();
-        const youtubeRegex=/^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
-        if(youtubeRegex.test(youtubeVideo)){
-            setYoutubeURL(youtubeVideo);
-            setYoutubeError('');
-        }
-        else{
-            setYoutubeError('Invalid youtube URL')
-        }
-        setYoutubeURL(youtubeVideo);
+  const handleYoutubeSubmit = (e) => {
+    e.preventDefault();
+    const youtubeRegex =
+      /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
+    if (youtubeRegex.test(youtubeVideo)) {
+      setYoutubeURL(youtubeVideo);
+      setYoutubeError("");
+    } else {
+      setYoutubeError("Invalid youtube URL");
     }
+    setYoutubeURL(youtubeVideo);
+  };
 
-
-
- return(
+  return (
     <div class="create">
-        <form>
-            <h1>Create Course</h1>
-            <label>Title</label>
-            <input type="text" onChange={handleChange} Placeholder="Enter the Title" name="title" value={video.title}></input>
-            <label>Description</label>
-            <textarea type="text" onChange={handleChange} Placeholder="Enter brief Description" name="description" value={video.description}></textarea>
-            <button onClick={publish} className="publish-button">Publish</button>    
+      <form>
+        <h1>Create Course</h1>
+        <label>Title</label>
+        <input
+          type="text"
+          onChange={handleChange}
+          Placeholder="Enter the Title"
+          name="title"
+          value={video.title}
+        ></input>
+        <label>Description</label>
+        <textarea
+          type="text"
+          onChange={handleChange}
+          Placeholder="Enter brief Description"
+          name="description"
+          value={video.description}
+        ></textarea>
+        <button onClick={publish} className="publish-button">
+          Publish
+        </button>
+      </form>
+      <div className="youtube-link">
+        <form onSubmit={handleYoutubeSubmit}>
+          <input
+            type="text"
+            Placeholder="Enter Youtube Url"
+            required
+            onChange={handleYoutubeChange}
+          ></input>
+          <button className="youtube-button">View</button>
         </form>
-        <div className="youtube-link">
-            <form onSubmit={handleYoutubeSubmit}>
-                <input type="text" Placeholder="Enter Youtube Url"  required
-                onChange={handleYoutubeChange} ></input>
-                <button className="youtube-button" >View</button>
-            </form>
-            {youtubeError && <div className='error-msg'>{youtubeError} </div> }
+        {youtubeError && <div className="error-msg">{youtubeError} </div>}
 
-            <div className="youtubeBox">
-                <ReactPlayer className="player" url={youtubeURL}  controls />
-            </div>
+        <div className="youtubeBox">
+          <ReactPlayer className="player" url={youtubeURL} controls />
         </div>
+      </div>
     </div>
-  
- )
+  );
 }
 
 export default Create;
--- client/src/register.js
+++ client/src/register.js
@@ -1,68 +1,78 @@
-import './css/style.css';
-import loginbg from './images/loginbg.jpg';
+import "./css/style.css";
+import loginbg from "./images/loginbg.jpg";
 import React, { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-function Register(){
-    const [user, setUser] = useState(
-        {
-            username: '',
-            email: '',
-            password: ''
-        }
-    )
+function Register() {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-    function handleChange(e) {
-        const {name, value} = e.target;
-        setUser(prevInput => {
-          return(
-            {
-              ...prevInput,
-              [name]: value
-            }
-          )
-        })
-      }
-    
-      async function createAccount(e) {
-        e.preventDefault();
-        
-        try{
-            const newUser = {
-                newUser : {
-            username: user.username,
-            email: user.email,
-            password: user.password,
-            video: []
-            }}
-            console.log(newUser);
-            await axios.post('/adduser', newUser);
-            alert("Account Created Please Login")
-        } catch(error){
-            console.log(error);
-        }
-        
-      }
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setUser((prevInput) => {
+      return {
+        ...prevInput,
+        [name]: value,
+      };
+    });
+  }
 
+  async function createAccount(e) {
+    e.preventDefault();
 
+    try {
+      const newUser = {
+        newUser: {
+          username: user.username,
+          email: user.email,
+          password: user.password,
+          video: [],
+        },
+      };
+      await axios.post("/adduser", newUser);
+      alert("Account Created Please Login");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+  return (
+    <div class="form">
+      <form>
+        <h1>Register</h1>
+        <label>Username</label>
+        <input
+          onChange={handleChange}
+          name="username"
+          value={user.username}
+          type="text"
+          Placeholder="Enter your Username"
+        ></input>
+        <label>Email</label>
+        <input
+          onChange={handleChange}
+          name="email"
+          value={user.email}
+          type="text"
+          Placeholder="Enter your Email"
+        ></input>
+        <label>Password</label>
+        <input
+          onChange={handleChange}
+          name="password"
+          value={user.password}
+          type="password"
+          Placeholder="Enter your Password"
+        ></input>
 
-    return(
-        <div class="form">
-            <form>
-                <h1>Register</h1>
-                <label>Username</label>
-                <input onChange={handleChange} name="username" value={user.username} type="text" Placeholder="Enter your Username"></input>
-                <label>Email</label>
-                <input onChange={handleChange} name="email" value={user.email}  type="text" Placeholder="Enter your Email"></input>
-                <label>Password</label>
-                <input onChange={handleChange} name="password" value={user.password}  type="password" Placeholder="Enter your Password"></input>
-
-                <button onClick={createAccount}>Register</button>
-            </form>
-            <img src={loginbg} alt="loginbg"/>
-        </div>
-    );
+        <button onClick={createAccount}>Register</button>
+      </form>
+      <img src={loginbg} alt="loginbg" />
+    </div>
+  );
 }
 
 export default Register;
