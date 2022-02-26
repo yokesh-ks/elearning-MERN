6f6f66c63389fc35b22531496185f3cb1c7e642e
YOKESH K S <ksyokesh98@gmail.com>
2022-02-27 00:08:25 +0530

    Code CleanUp (#1)

--- client/src/Components/hero.js
+++ client/src/Components/hero.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import '../css/style.css';
+import React from "react";
+import "../css/style.css";
 import herobg from "../images/hero-bg.jpg";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function hero() {
-    return (
-      <div className = "hero">
-          <div className = "hero-content">
-            <h1>Share what you know</h1>
-            <p>Create online courses and coaching services.</p>
-            <p> Transform your experience and know-how into a thriving knowledge business</p>
-            <button class="hero-btn"><Link className="link" to="/viewcourse">View-Course</Link></button>
-          </div>
-          <img src={herobg} alt="herobg"/>
-        
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1>Share what you know</h1>
+        <p>Create online courses and coaching services.</p>
+        <p>
+          {" "}
+          Transform your experience and know-how into a thriving knowledge
+          business
+        </p>
+        <button class="hero-btn">
+          <Link className="link" to="/viewcourse">
+            View-Course
+          </Link>
+        </button>
       </div>
-    );
-  }
-  
-  export default hero;
-  
+      <img src={herobg} alt="herobg" />
+    </div>
+  );
+}
+
+export default hero;
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import Hero from './Components/hero';
+import React from "react";
+import Hero from "./Components/hero";
 
 function Home() {
-    return (
-      <div class="container">
-        <Hero />
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+  return (
+    <div class="container">
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
